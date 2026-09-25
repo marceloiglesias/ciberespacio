3fa2d7accc131eb861a2000512602cbd4be60e19
marceloiglesias <104341536+marceloiglesias@users.noreply.github.com>
2026-09-25 14:09:24 +0000

    Update Contact_Zoho_API.jsx

--- src/components/Contact_Zoho_API.jsx
+++ src/components/Contact_Zoho_API.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
 
       // Enviar a Zoho Forms
       const response = await fetch(
-        'https://forms.zohopublic.com/julioiglesiascibere1/form/Services/formperma/r4-tTkLoQSSsRiTLlTSxQMwHXNNFGAbW24CaJGJSQhw/records',
+        'https://forms.zohopublic.com/julioiglesiascibere1/form/Services/formperma/r4-tTkLoQSSsRiTLlTSxQMwHXNNFGAbW24CaJGJSQhw/htmlRecords/submit',
         {
           method: 'POST',
           body: zohoFormData,
