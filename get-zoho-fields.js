f7ba6811d0ab4905cc1c89c80e94717f36e53aee
marceloiglesias <104341536+marceloiglesias@users.noreply.github.com>
2026-09-25 15:46:52 +0000

    Update get-zoho-fields.js

--- get-zoho-fields.js
+++ get-zoho-fields.js
@@ -3,7 +3,7 @@
  *
  * CÓMO USAR:
  * 1. Abre el formulario de Zoho en tu navegador:
- *    https://forms.zohopublic.com/julioiglesiascibere1/form/ContactUs/formperma/r4-tTkLoQSSsRiTLlTSxQMwHXNNFGAbW24CaJGJSQhw
+ *    https://forms.zohopublic.com/julioiglesiascibere1/form/ContactUs/formperma/leKSBkBe2hd2d36DKaOhKTTWDVMk17QHLapU1g_zCpk
  *
  * 2. Abre la consola del navegador (F12)
  *
